8446ada81e12c7538fe4bf0bfb6e83b1be8ee8e8
Lexi M <104409081+Babylex710@users.noreply.github.com>
2022-08-08 22:54:15 -0700

    added time blocks to html

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Day-Scheduler
+
+## Description
+
+A calendar application that allows the user to schedule events for each hour of the day.
+
+-Create a daily planner that has the current day displayed at the top of the calendar
+
+-The user is presented with time blocks 
+
+-The time blocks are color coded to indicate whether it is in the past, present, or future
+
+-User can save event and the input is stored in local storage
+
+-User can refresh page and events persist
+
+
+## Usage
+
+User can find deployed app at [GitHub](https://github.com/Babylex710/Work-Day-Scheduler)
+
+The following image shows the web application's appearance:
+![alt screenshot](./assets/Screen%20Shot%202022-08-07%20at%207.24.05%20PM.png)
---
+++ script.js
@@ -0,0 +1,62 @@
+//Makes each button independent from other buttons
+$(document).ready(function () {
+
+    // Displays current date 
+    $("#currentDay").text(moment().format("MMMM Do YYYY"));
+
+    //Function to save user input into local storage
+    $(".saveBtn").on("click", function () {
+        console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+    })
+
+    //Saves user input for each hour
+    $("#9am .description").val(localStorage.getItem("9am"));
+
+    $("#10am .description").val(localStorage.getItem("10am"));
+
+    $("#11am .description").val(localStorage.getItem("11am"));
+
+    $("#12pm .description").val(localStorage.getItem("12pm"));
+
+    $("#1pm .description").val(localStorage.getItem("1pm"));
+
+    $("#2pm .description").val(localStorage.getItem("2pm"));
+
+    $("#3pm .description").val(localStorage.getItem("3pm"));
+
+    $("#4pm .description").val(localStorage.getItem("4pm"));
+
+    $("#5pm .description").val(localStorage.getItem("5pm"));
+
+    //Funtion to color code time blocks to indicate whether it is in the past, present, or future
+    function hourSaved() {
+        var currentTime = moment().hour();
+
+        $(".time-block").each(function () {
+            var rowHour = parseInt($(this).attr("id").split("hour"));
+            console.log(rowHour, currentTime)
+
+            if (rowHour < currentTime) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (rowHour === currentTime) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    hourSaved();
+
+    setInterval(hourSaved, 60000)
+})
